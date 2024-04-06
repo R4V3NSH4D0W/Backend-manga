a45e7af7221ce97cfd68368cdc6758e6305cd7fe
R4V3NSH4D0W <lenishmagar@gmail.com>
2024-04-06 10:29:02 +0545

    minor fixes

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const search=require('./src/routes/search');
 const mangainfo=require('./src/routes/mangaInfo');
 const chapterspages=require('./src/routes/chapterPages');
 
-const animeInfo =require('./src/animeRoutes/animeInfo');
-const searchAnime =require('./src/animeRoutes/searchAnime');
-const animeSource =require('./src/animeRoutes/animeSource');
-const randomAnime =require('./src/animeRoutes/randomAnime');
-const popularAnime =require('./src/animeRoutes/popularAnime');
-const trendingAnime =require('./src/animeRoutes/trendingAnime');
-const episodeSource =require('./src/animeRoutes/episodeSource');
+// const animeInfo =require('./src/animeRoutes/animeInfo');
+// const searchAnime =require('./src/animeRoutes/searchAnime');
+// const animeSource =require('./src/animeRoutes/animeSource');
+// const randomAnime =require('./src/animeRoutes/randomAnime');
+// const popularAnime =require('./src/animeRoutes/popularAnime');
+// const trendingAnime =require('./src/animeRoutes/trendingAnime');
+// const episodeSource =require('./src/animeRoutes/episodeSource');
 
 
 const app =express();
@@ -25,14 +25,13 @@ app.use((req,res,next)=>{setTimeout(next,1000)});
 app.use('/api/search',search);
 app.use('/api/mangaInfo',mangainfo);
 app.use('/api/chapterPages',chapterspages);
-
-app.use('/api/animeInfo',animeInfo);
-app.use('/api/searchAnime',searchAnime);
-app.use('/api/episodeInfo',episodeInfo);
-app.use('/api/animeSource',animeSource);
-app.use('/api/randomAnime',randomAnime);
-app.use('/api/popularAnime',popularAnime);
-app.use('/api/trendingAnime',trendingAnime);
+// app.use('/api/animeInfo',animeInfo);
+// app.use('/api/searchAnime',searchAnime);
+// app.use('/api/animeSource',animeSource);
+// app.use('/api/randomAnime',randomAnime);
+// app.use('/api/popularAnime',popularAnime);
+// app.use('/api/episodeSource',episodeSource);
+// app.use('/api/trendingAnime',trendingAnime);
 
 if(app.get('env')==='development'){
     app.use(morgan('tiny'));
